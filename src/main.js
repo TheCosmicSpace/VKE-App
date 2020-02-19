ab5c76ff1299b7dcb4f491a8b9d8d17ffd1b7492
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-02-19 15:41:11 +0300

    initial commit

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
+import Vuesax from 'vuesax'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+import 'material-icons/iconfont/material-icons.css';
+Vue.use(Vuesax, {
+  // options here
+})
 
 Vue.config.productionTip = false
 
