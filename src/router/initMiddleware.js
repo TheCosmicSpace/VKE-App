7635105a223076d2466b70946a06cf73a8bc177b
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-18 14:12:22 +0300

    initialization middleware is modified

--- src/router/initMiddleware.js
+++ src/router/initMiddleware.js
@@ -1,15 +1,11 @@
 import middleware from "@/middleware/index.js"
-
+/**
+ * 
+ * @param {*} router 
+ */
 export default function(router){
-    router.beforeEach((to, from, next) =>{
-      if(typeof to.meta.middleware === 'undefined') 
-        next();
-      else 
-        middleware({
-          meta: to.meta.middleware,
-          to,
-          from,
-          next
-        })
-    });  
+  router.beforeEach((to, from, next) => {
+    if(!to.meta.middleware) next();
+    else middleware({ meta: to.meta.middleware, to, from, next });
+  });  
 }
