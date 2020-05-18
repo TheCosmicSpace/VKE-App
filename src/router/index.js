26ff276818a81990244acc81f082c5f8ddd10884
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-18 14:24:49 +0300

    Routes path added

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Posts from '../views/Posts.vue'
 
 import initMiddleware from "./initMiddleware.js"
 
@@ -9,16 +9,35 @@ Vue.use(VueRouter)
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Posts',
+    component: Posts,
+    meta: {
+      middleware: ['checkMobileDevice']
+    },
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/posts/:postId',
+    name: 'PostItem',
+    props: true,
+    component: () => import('../views/PostItem.vue')
+  },
+  {
+    path: '/add-post',
+    name: 'AddPost',
+    component: () => import('../views/AddPost.vue')
+  },
+  {
+    path: '/auth',
+    name: 'Auth',
+    component: () => import('../views/Auth.vue')
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: {
+      middleware: ['isAuth']
+    },
+    component: () => import('../views/Profile.vue')
   }
 ]
 
