4be7b093b4387df49d392a6ffe81f9f52f95b596
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-18 14:30:22 +0300

    Vuex store includes modules

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,15 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+// Modules
+import user from './modules/user'
+import post from './modules/post'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-  }
+    user,
+    post
+  },
+  strict: process.env.NODE_ENV !== 'production'
 })
