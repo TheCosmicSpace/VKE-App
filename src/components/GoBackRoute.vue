71c2311ed7a072b46a5fb66e81dded00ec00e7d7
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-19 21:18:39 +0300

    GoBackRoute component created

---
+++ src/components/GoBackRoute.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <vs-button
+        @click="goBackRoute"
+        color="#d5397b"
+        circle
+        icon
+        floating
+        class="backRoute">
+        <i class='bx bx-left-arrow-alt' ></i>
+      </vs-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods:{
+    goBackRoute(){
+      this.$router.go(-1)
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+</style>
