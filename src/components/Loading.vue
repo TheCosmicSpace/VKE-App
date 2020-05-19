c23ff46f00364d84bbd1230d2d7cece6ff692791
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-19 21:42:57 +0300

    Loading component added

---
+++ src/components/Loading.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="center">
+    <div ref="content" class="content-div"></div>
+  </div>
+</template> 
+<script>
+  const loadingTypeMap = ["points", "default", "scale", "circles"]
+  export default {
+    computed: {
+      getRandomTypeLoading(){
+        return loadingTypeMap[Math.random() * loadingTypeMap.length | 0]
+      }
+    },
+    methods: {
+      openLoading() {
+        const loading = this.$vs.loading({
+          target: this.$refs.content,
+          type: this.getRandomTypeLoading,
+          color: "#d5397b"
+        })
+      }
+    },
+    mounted(){
+      this.openLoading()
+    }
+  }
+</script>
+
+
+<style lang="scss">
+  @import '@/style/LoadingComponent/style.scss';
+</style>
+
+<style lang="scss" scoped>
+  @import '@/style/LoadingComponent/style_scoped.scss';
+</style>
