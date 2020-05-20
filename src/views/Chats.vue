2524c9e1bd56e9a8ea90a67e732d7c62dd15bc5c
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-20 20:54:46 +0300

    Chats page added

---
+++ src/views/Chats.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="chats">
+    <!-- Title -->
+    <h2 class="chats-title page-title">
+      <span class="page-title__text">Chats</span>
+    </h2>
+    <!-- Toggle Rooms -->
+    <Search/>
+  </div>
+</template>
+
+<script>
+import Search from '@/components/Search.vue'
+  export default {
+    name: 'Chats',
+    components: {
+      Search
+    },
+    data:()=>({
+      active: 0,
+    })
+  }
+</script>
+
+
+<style lang="scss" >
+  @import '@/style/ChatsPage/style.scss';
+</style>
+
+<style lang="scss" scoped>
+  @import '@/style/ChatsPage/style_scoped.scss';
+</style>
