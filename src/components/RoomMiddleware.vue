855ffdc02e7717a1fef8f1c6bf40de991e7375fd
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-21 19:04:21 +0300

    RoomMiddleware component added

---
+++ src/components/RoomMiddleware.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <div class="key-input">
+      <vs-input v-model="key" autocomplete="off" placeholder="Secret Key"/>
+      <vs-button
+        @click="checkToSecretKey"
+        :loading="secretKeySending"
+        icon>
+        <i class='bx bxs-send'></i>
+      </vs-button>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    methods: {
+      checkToSecretKey(){
+
+      }
+    },
+    data:()=>({
+      secretKeySending: false,
+      key: ''
+    })
+  }
+</script>
+
+
+<style lang="scss">
+  @import '@/style/RoomMiddlewareComponent/style.scss';
+</style>
+
+<style lang="scss" scoped>
+  @import '@/style/RoomMiddlewareComponent/style_scoped.scss';
+</style>
