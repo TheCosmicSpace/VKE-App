dc156f89d1b286c28c7283b87eda6d496fb4f7d0
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-21 23:54:47 +0300

    Vuex chat module get rooms actions added

--- src/store/modules/chat.js
+++ src/store/modules/chat.js
@@ -14,6 +14,8 @@ export default {
   mutations: {
   },
   actions: {
+  // ===== Set actions ===== //
+
     // Upload Room Photo
     async uploadRoomPhoto({commit}, photo){
       const storageRef = await firebase.storage().ref(`chatsPhoto/${UUID()}`).put(photo)
@@ -34,6 +36,35 @@ export default {
       const docRef = await firebase.firestore().collection('chats').add(data)
       console.log(docRef);
       return docRef.id
+    },
+
+  //===== Get actions ===== //
+    // Get Chat Rooms
+    async getChatRooms({commit, dispatch}, typeRooms){
+      if(typeRooms === "scoped")
+        await dispatch("getScopedRooms")
+      else if(typeRooms === "global")
+        await dispatch("getGlobalRooms")
+    },
+    // Get Scoped Rooms
+    async getScopedRooms({dispatch, getters}){
+      const { id } = getters.getUser
+      console.log("getScopedRooms");
+      const chatsRef = await firebase.firestore().collection('chats')
+      const scopedRooms = await chatsRef.where("users", "array-contains", id)
+      // const scopedRoomsCollection = await dispatch("constructCollection", scopedRooms)
+      console.log(scopedRooms);
+    },
+    // Get Global Rooms
+    async getGlobalRooms({dispatch}){
+      console.log("getGlobalRooms");
+      const globalRooms = await firebase.firestore().collection('chats').get()
+      const globalRoomsCollection = await dispatch("constructCollection", globalRooms)
+      console.log(globalRoomsCollection);
+    },
+    // Construct Room Collections
+    async constructCollection({commit}, rooms){
+      return await rooms.docs.map(doc => ({... doc.data(), id: doc.id}))
     }
   },
   getters: {
