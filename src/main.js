a860c8ae9fbc48a14b43e6643c98e1381b6fa095
TheAdminPro <45514836+TheAdminPro@users.noreply.github.com>
2020-05-27 03:29:42 +0300

    init firebase & add event before install prompt

--- src/main.js
+++ src/main.js
@@ -1,20 +1,39 @@
 import Vue from 'vue'
+import VueChatScroll from 'vue-chat-scroll'
 import Vuesax from 'vuesax'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
 
+import firebase from 'firebase/app'
+import 'firebase/auth' 
+import firebaseConfig from './firebase.config'
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 import 'vuesax/dist/vuesax.css' //Vuesax styles
-import 'material-icons/iconfont/material-icons.css';
-Vue.use(Vuesax, {
-  // options here
-})
+import 'material-icons/iconfont/material-icons.css'
+
+Vue.use(Vuesax, {})
+Vue.use(VueChatScroll)
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+  created(){
+    this.$store.commit('setDeviseDetector')
+    // Save session
+    firebase.auth().onAuthStateChanged(async user => {
+      await this.$store.dispatch("stateChanged", user)
+    })
+  },
 }).$mount('#app')
+
+window.addEventListener('beforeinstallprompt', e => {
+  e.preventDefault()
+  store.commit('setInstallPrompt', e)
+})
